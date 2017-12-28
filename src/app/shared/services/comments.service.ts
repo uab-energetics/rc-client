a7c802ae605414392d8ccae64f9246adb7f51bdd
Chris Rocco <chris.rocco7@gmail.com>
2017-12-27 19:30:13 -0600

    comments service

--- src/app/shared/services/comments.service.ts
+++ src/app/shared/services/comments.service.ts
@@ -1,23 +1,43 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {AppComment} from "../../models/AppComment";
+import {AppChannel} from "../../models/AppChannel";
+
+const api = environment.api;
 
 @Injectable()
 export class CommentsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  commentInChannel(channel_name: string, message: string){
+  commentInChannel(channel_name: string, message: string): Observable<AppComment> {
+    return this.http.post<AppComment>(`${api}/channels/${channel_name}/comments`, {
+      message
+    })
   }
 
-  replyToComment(parent_id: number, message: string){
+  replyToComment(parent_id: number, message: string): Observable<AppComment> {
+    return this.http.post<AppComment>(`${api}/comments/${parent_id}/reply`, {
+      message
+    });
   }
 
-  editComment(id: number, message: string){
+  editComment(id: number, message: string): Observable<AppComment> {
+    return this.http.put<AppComment>(`${api}/comments/${id}`, {
+      message
+    });
   }
 
-  deleteComment(id: number){
+  deleteComment(id: number): Observable<any> {
+    return this.http.delete(`${api}/comments/${id}`);
   }
 
-  getChannel(channel_name: string){
+  getChannel(channel_name: string): Observable<AppChannel> {
+    return this.http.get<AppChannel>(`${api}/channels/${channel_name}`);
   }
 
 }
