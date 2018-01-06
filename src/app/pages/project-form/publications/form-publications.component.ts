e4233b39a5bf010fe69cbdecc07848161e27fa8c
Caleb Falcione <caleb.falcione@gmail.com>
2018-01-05 21:35:32 -0600

    Added publication csv upload to FormPublicationsComponent

--- src/app/pages/project-form/publications/form-publications.component.ts
+++ src/app/pages/project-form/publications/form-publications.component.ts
@@ -6,7 +6,7 @@ import {AppPublication} from "../../../models/AppPublication";
 import {ProjectService} from "../../../shared/services/project.service";
 import {PublicationsService} from "../../../shared/services/publications.service";
 import {NotifyService} from "../../../shared/services/notify.service";
-import {loadingPipe} from '../../../shared/helpers';
+import * as PapaParse from 'papaparse';
 import {ProjectFormService} from "../../../shared/services/project-form.service";
 import {AppForm} from "../../../models/AppForm";
 import {AppFormPublication} from "../../../models/AppFormPublication";
@@ -24,6 +24,8 @@ export class FormPublicationsComponent {
   loading = 0;
 
   publications: AppFormPublication[];
+
+  selectedFile: File;
 
   /* UI Data */
   modal;
@@ -50,14 +52,15 @@ export class FormPublicationsComponent {
   onPublicationFormSubmit(publications: AppPublication[], priority: number){
     this.modal.close();
     this.loading++;
-    this.projectFormService.addPublications(this.project, this.form, publications, priority)
+    let pubIDs = this.publications.map(pub => pub.id);
+    this.projectFormService.addPublications(this.project, this.form, pubIDs, priority)
       .finally(() => this.loading--)
       .subscribe(() => this.loadPublications())
   }
 
   onDeletePublication(publication){
     this.projectFormService.removePublication(this.project, this.form, publication)
-      .pipe(loadingPipe.bind(this))
+      .finally(() => this.loading--)
       .subscribe(() => {
         this.notify.toast('Publication removed');
         this.loadPublications();
@@ -72,6 +75,34 @@ export class FormPublicationsComponent {
   addProjectPublications() {
     this.projectFormService.inheritProjectPublications(this.project, this.form)
       .subscribe(() => this.loadPublications());
+  }
+
+  handleFileInput = (fileList: FileList) => this.selectedFile = fileList[0];
+
+  onUploadFile(){
+    if(!this.selectedFile)
+      return alert('No file selected!');
+
+    PapaParse.parse(this.selectedFile, {
+      skipEmptyLines: true,
+      delimiter: ",", //specified because papaparse will throw errors for one-column csvs otherwise
+      complete: (results, file) => {
+        console.log("Parsing complete:", results, file);
+        if(results.errors.length > 0)
+          return alert('Could not parse file. Please check format and try again. (Details in console)');
+        let pubIds = results.data.map(row => row[0]);
+        this.loading++;
+        this.projectFormService.addPublications(this.project, this.form, pubIds)
+          .finally(() => this.loading--)
+          .subscribe( res => {
+            this.notify.toast('CSV Uploaded');
+            this.loadPublications();
+          }, err => {
+            this.notify.alert('Error', err.error.details, 'error');
+            return []
+          });
+      }
+    });
   }
 
 
