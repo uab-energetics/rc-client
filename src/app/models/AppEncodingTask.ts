7ddeac08b8e5ed1dc8c89c6a24a9a8f7e20aa115
Caleb <caleb.falcione@gmail.com>
2018-05-14 15:06:55 -0500

    fixes TaskList table

--- src/app/models/AppEncodingTask.ts
+++ src/app/models/AppEncodingTask.ts
@@ -1,9 +1,15 @@
 import {AppExperimentEncoding} from "../core/encodings/AppExperimentEncoding";
+import {AppPublication} from "../core/publications/AppPublication";
+import {AppForm} from "../core/forms/AppForm";
 
 export interface AppEncodingTask {
   id: number,
   encoding_id: number,
   encoding?: AppExperimentEncoding,
+  publication_id: number,
+  publication?: AppPublication,
+  form_id: number,
+  form: AppForm,
   active: boolean,
   complete: boolean
 }
