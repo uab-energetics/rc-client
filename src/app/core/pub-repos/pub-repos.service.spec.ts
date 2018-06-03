afde27c866651eae1d986451fc5612ae8fb1a267
Chris Rocco <chris.rocco7@gmail.com>
2018-06-03 12:24:53 -0500

    added pub-repos service skeleton

--- src/app/core/pub-repos/pub-repos.service.spec.ts
+++ src/app/core/pub-repos/pub-repos.service.spec.ts
@@ -1,17 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { PubReposService } from './pub-repos.service';
+import {HttpClient} from "@angular/common/http";
 
 describe('PubReposService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PubReposService]
+      providers: [
+        HttpClient,
+        PubReposService
+      ]
     });
   });
 
-  it('should be created', inject([PubReposService], (service: PubReposService) => {
-
-
-
-  }));
+  it('should be created', async () => {
+    let pubRepos = TestBed.get(PubReposService)
+    expect(pubRepos).toBeTruthy()
+  });
 });
--- src/app/core/pub-repos/pub-repos.service.ts
+++ src/app/core/pub-repos/pub-repos.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import {PubRepo} from "./PubRepo";
 import {Publication} from "./Publication";
 import {HttpClient} from "@angular/common/http";
+import {environment as env} from "../../../environments/environment";
 
 @Injectable()
 export class PubReposService {
 
   constructor(private http: HttpClient) { }
 
-  createRepo(data: PubRepo) {}
+  createRepo(projectID: number, data: PubRepo) {
+    return this.http.post(`${env.api}/projects/${projectID}/pub-repos`, data)
+  }
 
   updateRepo(id: string, data: PubRepo) {}
 
