f46f36a07c163943ed7ec87d2c51db56f8129243
Chris Rocco <chris.rocco7@gmail.com>
2018-06-04 13:57:10 -0500

    added instances sidebar

--- src/app/pages/main/pub-repos/pub-repos.component.ts
+++ src/app/pages/main/pub-repos/pub-repos.component.ts
@@ -1,33 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PubReposService} from "../../../core/pub-repos/pub-repos.service";
 import {ActiveProjectService} from "../../../core/active-project/active-project.service";
 import {AppProject} from "../../../core/projects/AppProject";
 import {PubRepo} from "../../../core/pub-repos/PubRepo";
+import {Publication} from "../../../core/pub-repos/Publication";
+import {PageAsideComponent} from "../../shared/page-aside/PageAsideComponent";
 
 @Component({
   selector: 'app-pub-repos',
   templateUrl: './pub-repos.component.html',
   styleUrls: ['./pub-repos.component.scss']
 })
-export class PubReposComponent implements OnInit {
+export class PubReposComponent extends PageAsideComponent implements OnInit, OnDestroy {
 
+  activeRepo: PubRepo
   repos: PubRepo[]
+  activeRepoPublications: Publication[]
+
   p: number = 1
 
-  constructor(
-    public pubReposService: PubReposService,
-    public ps: ActiveProjectService
-  ) {
-  }
+  constructor(public repoService: PubReposService, public ps: ActiveProjectService ) {
+    super()
 
-  ngOnInit() {
-    this.pubReposService.repos$
+    this.repoService.repos$
       .subscribe(R => this.repos = R)
 
     this.ps.project$.subscribe((project: AppProject) => {
-      this.pubReposService.requestRepos(project.id)
+      this.repoService.requestRepos(project.id)
     })
-
   }
 
 }
