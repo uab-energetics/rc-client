d6a355a20c0320b7758d0bccce0b6cf1364851e9
Chris Rocco <chris.rocco7@gmail.com>
2018-06-08 02:05:58 -0500

    data mutation error fix!

--- src/app/lib/rc-forms/form-filler/formReducer.ts
+++ src/app/lib/rc-forms/form-filler/formReducer.ts
@@ -1,12 +1,12 @@
-import {DynamicForm} from "./DynamicForm";
-import {FormEvent} from "./events/FormEvent";
-import {INPUT_EDITED} from "./events/InputEdited";
-import * as lodash from "lodash";
+import {DynamicForm} from "./DynamicForm"
+import {FormEvent} from "./events/FormEvent"
+import {RESPONSE_UPDATED} from "./events/InputEdited"
+import * as lodash from "lodash"
 
 export const reduceForm = (form: DynamicForm, event: FormEvent): DynamicForm => {
 
-  if(event.type === INPUT_EDITED) {
-    let copy = { ...form }
+  if(event.type === RESPONSE_UPDATED) {
+    let copy = JSON.parse(JSON.stringify(form))
     lodash.set(copy, event.payload.key.split('.'), event.payload.data)
     return copy
   }
