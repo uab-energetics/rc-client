0743e2a60c40fb8a761fe09893da1f3cdc12644f
Chris Rocco <chris.rocco7@gmail.com>
2018-06-07 21:05:35 -0500

    hotkeys demo

--- src/app/pages/main/pub-coder-4/pub-coder-4.component.ts
+++ src/app/pages/main/pub-coder-4/pub-coder-4.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import * as lodash from 'lodash'
 import {Subject} from "rxjs/Subject"
+import {NotifyService} from "../../../core/notifications/notify.service";
 
 @Component({
   selector: 'app-pub-coder-4',
@@ -14,7 +15,10 @@ export class PubCoder4Component implements OnInit {
   events = []
   changes$ = new Subject()
 
+  constructor(public notify: NotifyService) {}
+
   ngOnInit() {
+    this.registerHotKeys()
     this.changes$.subscribe( event => this.events.push(event) )
   }
 
@@ -27,5 +31,17 @@ export class PubCoder4Component implements OnInit {
       }
     }
     return events.reduce(reducer, state)
+  }
+
+  private registerHotKeys(){
+    document.addEventListener("keydown", event => {
+      if(event.ctrlKey && event.key === "s"){
+        event.preventDefault();
+        // make this a save event
+        this.encoding = this.reduceEncoding(this.encoding, this.events)
+        this.events = []
+        this.notify.toast('Encoding Saved!')
+      }
+    })
   }
 }
