9a5b4434aa2418dc3e63578ea5bb759c60b3cbfa
Chris Rocco <chris.rocco7@gmail.com>
2018-06-07 23:53:16 -0500

    removed shared dependencies from forms module

--- src/app/lib/rc-forms/rc-forms.module.ts
+++ src/app/lib/rc-forms/rc-forms.module.ts
@@ -5,22 +5,36 @@ import {FormGroupComponent} from "./components/form-group/form-group.component";
 import {FormControlComponent} from "./components/form-control/form-control.component";
 import {SharedModule} from "../../core/shared.module";
 import {AngularMaterialModule} from "../../core/auth/angular-material/angular.material.module";
+import {SelectControl} from "./components/controls/select/select.component";
+import {TextControl} from "./components/controls/text/text.component";
+import {NumberControl} from "./components/controls/number/number.component";
+import {MultiSelectControl} from "./components/controls/multi-select/multi-select.component";
+import {BoolControl} from "./components/controls/bool/bool.component";
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule,
-    AngularMaterialModule,
+    AngularMaterialModule
   ],
   declarations: [
     FormGroupListComponent,
     FormGroupComponent,
-    FormControlComponent
+    FormControlComponent,
+    SelectControl,
+    MultiSelectControl,
+    NumberControl,
+    BoolControl,
+    TextControl
   ],
   exports: [
     FormGroupListComponent,
     FormGroupComponent,
-    FormControlComponent
+    FormControlComponent,
+    SelectControl,
+    MultiSelectControl,
+    NumberControl,
+    BoolControl,
+    TextControl
   ]
 })
 export class RcFormsModule { }
