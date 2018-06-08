2f089ba36a8b6361289ba90dce23af93337cd350
Chris Rocco <chris.rocco7@gmail.com>
2018-06-07 23:56:17 -0500

    explict import of angular modules - rc-forms is now a 100% standalone module (with one peer-dependency)

--- src/app/lib/rc-forms/rc-forms.module.ts
+++ src/app/lib/rc-forms/rc-forms.module.ts
@@ -3,18 +3,59 @@ import {CommonModule} from '@angular/common';
 import {FormGroupListComponent} from "./components/form-group-list/form-group-list.component";
 import {FormGroupComponent} from "./components/form-group/form-group.component";
 import {FormControlComponent} from "./components/form-control/form-control.component";
-import {SharedModule} from "../../core/shared.module";
-import {AngularMaterialModule} from "../../core/auth/angular-material/angular.material.module";
 import {SelectControl} from "./components/controls/select/select.component";
 import {TextControl} from "./components/controls/text/text.component";
 import {NumberControl} from "./components/controls/number/number.component";
 import {MultiSelectControl} from "./components/controls/multi-select/multi-select.component";
 import {BoolControl} from "./components/controls/bool/bool.component";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from "@angular/material";
+import {CdkTableModule} from "@angular/cdk/table";
 
 @NgModule({
   imports: [
     CommonModule,
-    AngularMaterialModule
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSidenavModule,
+    CdkTableModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatToolbarModule
   ],
   declarations: [
     FormGroupListComponent,
