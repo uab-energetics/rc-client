496bf3cce8ea00d51f5b2d83b5acbeb46b915a93
Chris Rocco <chris.rocco7@gmail.com>
2019-07-27 18:25:27 -0500

    Updated authentication pages to use firebaseui component.

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/auth/auth.service';
+import { RedirectService } from '../../core/auth/redirect.service';
+import { NotifyService } from '../../core/notifications/notify.service';
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +14,36 @@ import { Component, OnInit } from '@angular/core'
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
+
+  login(provider: string) {
+    let P = null;
+    if (provider === 'github') P = new firebase.auth.GithubAuthProvider()
+    if (provider === 'facebook') P = new firebase.auth.FacebookAuthProvider()
+    return this.afAuth.auth.signInWithPopup(P)
+      .then(() => this.afAuth.auth.currentUser.getIdToken(true))
+      .then((idToken) => console.log(idToken))
+  }
 
   ngOnInit() {
+    document.body.classList.add('page-login-v3', 'layout-full')
+    // FirebaseUI config.
+    var uiConfig = {
+      signInSuccessUrl: window.location.origin + '/callbacks/oauth',
+      signInOptions: [
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ]
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-outlet', uiConfig);
+  }
+
+  ngOnDestroy(): void {
+    document.body.classList.remove('page-login-v3', 'layout-full')
   }
 
 }
