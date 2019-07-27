0d646c64337f8a4d718119cfd8929cbe1baa035e
Chris Rocco <chris.rocco7@gmail.com>
2019-07-27 18:34:13 -0500

    Updated production .env file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,8 @@
 
 let environment = {
   api: 'https://api.researchcoder.com',
-  disableRouteGuards: true,
-  showLog: true,
-  production: false,
+  disableRouteGuards: false,
+  production: true,
   callbacks: {
     redeemResearcherInvite: window.location.origin + "/callbacks/redeem-researcher-invite",
     redeemEncoderInvite: window.location.origin + "/callbacks/redeem-encoder-invite"
@@ -18,22 +17,16 @@ let environment = {
       cluster: 'us2',
       encrypted: true
     }
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyBbQJlFfdN_bLkg-lWRdqR0F4hzWTH0VOY",
+    authDomain: "meta-research-data-portal.firebaseapp.com",
+    databaseURL: "https://meta-research-data-portal.firebaseio.com",
+    projectId: "meta-research-data-portal",
+    storageBucket: "",
+    messagingSenderId: "596453631490",
+    appId: "1:596453631490:web:485fdb333f99eaeb"
   }
-};
-
-if(localStorage.env){
-  let custom = JSON.parse(localStorage.env);
-  Object.assign(environment, custom);
-  console.info('Overriding environment..');
-}
-
-
-window['setEnv'] = (key, value) => {
-  let env = {};
-  if(localStorage.env) env = JSON.parse(localStorage.env);
-  env[key] = value;
-  localStorage.env = JSON.stringify(env);
-  window.location.reload();
 };
 
 export { environment }
