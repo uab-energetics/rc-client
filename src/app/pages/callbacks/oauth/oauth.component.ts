3753df9ec1a8925902a82c6ce4daa0b7e246a54b
Chris Rocco <chris.rocco7@gmail.com>
2019-07-27 21:08:07 -0500

    Removed dead code and UAB branding.

--- src/app/pages/callbacks/oauth/oauth.component.ts
+++ src/app/pages/callbacks/oauth/oauth.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../../core/auth/auth.service";
 import { RedirectService } from "../../../core/auth/redirect.service";
-import { ActivatedRoute, Router } from "@angular/router";
-
-import * as firebase from 'firebase';
 import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-oauth',
@@ -13,18 +11,18 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class OauthComponent implements OnInit {
 
-  constructor(private as: AuthService,
+  constructor(
+    private authService: AuthService,
     private redirectService: RedirectService,
-    public afAuth: AngularFireAuth,
-    private route: ActivatedRoute) { }
+    public afAuth: AngularFireAuth
+  ) { }
 
   ngOnInit() {
     firebase.auth().getRedirectResult()
       .then(() => this.afAuth.auth.currentUser.getIdToken(true))
       .then((idToken) => {
-        this.as.setToken(idToken)
+        this.authService.setToken(idToken)
         this.redirectService.followRedirect()
       })
   }
-
 }
