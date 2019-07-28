561a79365ab804d488156a16c349b2476b1a3ce8
Chris Rocco <chris.rocco7@gmail.com>
2019-07-28 11:29:51 -0500

    Removed the failed pipelines feature pages.

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -1,56 +1,55 @@
-import {mainRoutes} from './mainRoutes'
-import {AngularMaterialModule} from '../../core/auth/angular-material/angular.material.module'
-import {CommonModule} from '@angular/common'
-import {SharedModule} from '../../core/shared.module'
-import {NgModule} from '@angular/core'
-import {FormsModule} from '@angular/forms'
-import {RouterModule} from '@angular/router'
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
-import {TreeModule} from 'angular-tree-component'
-import {PagesComponent} from '../page.component'
-import {MainComponent} from './main.component'
-import {MyTasksPageComponent} from './my-tasks-page/my-tasks-page.component'
-import {ProfileComponent} from './settings/profile/profile.component'
-import {ProjectsComponent} from './projects/projects.component'
-import {InsightsComponent} from './insights/insights.component'
-import {AccountComponent} from './settings/account/account.component'
-import {SettingsComponent} from './settings/settings.component'
-import {CreateProjectComponent} from './create-project/create-project.component'
-import {FormBuilderComponent} from './form-builder/form-builder.component'
-import {ProjectFormsComponent} from './project-forms/project-forms.component'
-import {ProjectFormPageComponent} from './project-form/project-form-page.component'
-import {FormPublicationsComponent} from './project-form/publications/form-publications.component'
-import {ProjectPublicationsComponent} from './project-publications/project-publications.component'
-import {PubCoderComponent} from './pub-coder/pub-coder.component'
-import {ExperimentFormComponent} from './pub-coder/experiment-form/experiment-form.component'
-import {ConflictsComponent} from './conflicts/conflicts.component'
-import {CollaboratorsComponent} from './project-users/collaborators/collaborators.component'
-import {ProjectFormSettingsComponent} from './project-form/settings/project-form-settings.component'
-import {FormEncodersComponent} from './project-form/encoders/encoders.component'
-import {DiscussComponent} from './discuss/discuss.component'
-import {WelcomeComponent} from './welcome/welcome.component'
-import {CodeBookTreeComponent} from './form-builder/codebook-tree/codebook-tree.component'
-import {EncodingTreeComponent} from './pub-coder/encoding-tree/encoding-tree.component'
-import {StructureEditorComponent} from './pub-coder/structure-editor/structure-editor.component'
-import {EncodersComponent} from './project-users/collaborators/encoders.component'
-import {SidebarModule} from '../shared/sidebar/sidebar.module'
-import {ActiveProjectModule} from '../../core/active-project/active-project.module'
-import {ProjectUsersComponent} from './project-users/project-users.component'
-import {ProjectDashboardComponent} from './project-dashboard/project-dashboard.component'
-import {ProjectsModule} from '../../core/projects/projects.module'
-import {MatTableModule} from '@angular/material'
-import {TasksModule} from "../../core/tasks/tasks.module"
-import {PubReposComponent} from './pub-repos/pub-repos.component'
-import {NgxPaginationModule} from 'ngx-pagination'
-import {PubReposModule} from "../../core/pub-repos/pub-repos.module";
-import {PapaParseModule} from 'ngx-papaparse';
-import {UploadPreviewComponent} from './pub-repos/upload-preview/upload-preview.component';
-import {AddOneComponent} from './pub-repos/add-one/add-one.component';
-import {PmcModule} from "../../core/pmc/pmc.module";
-import {PmcImporterComponent} from './pub-repos/pmc-importer/pmc-importer.component';
-import {PipesComponent} from './pipes/pipes.component';
-import {NavbarModule} from "../shared/navbar/navbar.module";
-import {CodebooksModule} from "../../core/codebooks/codebooks.module";
+import { mainRoutes } from './mainRoutes'
+import { AngularMaterialModule } from '../../core/auth/angular-material/angular.material.module'
+import { CommonModule } from '@angular/common'
+import { SharedModule } from '../../core/shared.module'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { TreeModule } from 'angular-tree-component'
+import { PagesComponent } from '../page.component'
+import { MainComponent } from './main.component'
+import { MyTasksPageComponent } from './my-tasks-page/my-tasks-page.component'
+import { ProfileComponent } from './settings/profile/profile.component'
+import { ProjectsComponent } from './projects/projects.component'
+import { InsightsComponent } from './insights/insights.component'
+import { AccountComponent } from './settings/account/account.component'
+import { SettingsComponent } from './settings/settings.component'
+import { CreateProjectComponent } from './create-project/create-project.component'
+import { FormBuilderComponent } from './form-builder/form-builder.component'
+import { ProjectFormsComponent } from './project-forms/project-forms.component'
+import { ProjectFormPageComponent } from './project-form/project-form-page.component'
+import { FormPublicationsComponent } from './project-form/publications/form-publications.component'
+import { ProjectPublicationsComponent } from './project-publications/project-publications.component'
+import { PubCoderComponent } from './pub-coder/pub-coder.component'
+import { ExperimentFormComponent } from './pub-coder/experiment-form/experiment-form.component'
+import { ConflictsComponent } from './conflicts/conflicts.component'
+import { CollaboratorsComponent } from './project-users/collaborators/collaborators.component'
+import { ProjectFormSettingsComponent } from './project-form/settings/project-form-settings.component'
+import { FormEncodersComponent } from './project-form/encoders/encoders.component'
+import { DiscussComponent } from './discuss/discuss.component'
+import { WelcomeComponent } from './welcome/welcome.component'
+import { CodeBookTreeComponent } from './form-builder/codebook-tree/codebook-tree.component'
+import { EncodingTreeComponent } from './pub-coder/encoding-tree/encoding-tree.component'
+import { StructureEditorComponent } from './pub-coder/structure-editor/structure-editor.component'
+import { EncodersComponent } from './project-users/collaborators/encoders.component'
+import { SidebarModule } from '../shared/sidebar/sidebar.module'
+import { ActiveProjectModule } from '../../core/active-project/active-project.module'
+import { ProjectUsersComponent } from './project-users/project-users.component'
+import { ProjectDashboardComponent } from './project-dashboard/project-dashboard.component'
+import { ProjectsModule } from '../../core/projects/projects.module'
+import { MatTableModule } from '@angular/material'
+import { TasksModule } from "../../core/tasks/tasks.module"
+import { PubReposComponent } from './pub-repos/pub-repos.component'
+import { NgxPaginationModule } from 'ngx-pagination'
+import { PubReposModule } from "../../core/pub-repos/pub-repos.module";
+import { PapaParseModule } from 'ngx-papaparse';
+import { UploadPreviewComponent } from './pub-repos/upload-preview/upload-preview.component';
+import { AddOneComponent } from './pub-repos/add-one/add-one.component';
+import { PmcModule } from "../../core/pmc/pmc.module";
+import { PmcImporterComponent } from './pub-repos/pmc-importer/pmc-importer.component';
+import { NavbarModule } from "../shared/navbar/navbar.module";
+import { CodebooksModule } from "../../core/codebooks/codebooks.module";
 
 @NgModule({
   imports: [
@@ -104,7 +103,6 @@ import {CodebooksModule} from "../../core/codebooks/codebooks.module";
     ProjectDashboardComponent,
     PubReposComponent,
     UploadPreviewComponent,
-    PipesComponent,
     AddOneComponent,
     PmcImporterComponent,
   ],
--- src/app/pages/main/mainRoutes.ts
+++ src/app/pages/main/mainRoutes.ts
@@ -1,117 +1,112 @@
-import {Routes} from "@angular/router";
-import {MainComponent} from "./main.component";
-import {SettingsComponent} from "./settings/settings.component";
-import {ProfileComponent} from "./settings/profile/profile.component";
-import {ProjectsComponent} from "./projects/projects.component";
-import {CreateProjectComponent} from "./create-project/create-project.component";
-import {ProjectFormPageComponent} from "./project-form/project-form-page.component";
-import {FormBuilderComponent} from "./form-builder/form-builder.component";
-import {PubCoderComponent} from "./pub-coder/pub-coder.component";
-import {ConflictsComponent} from "./conflicts/conflicts.component";
-import {DiscussComponent} from "./discuss/discuss.component";
-import {MyTasksPageComponent} from "./my-tasks-page/my-tasks-page.component";
-import {WelcomeComponent} from "./welcome/welcome.component";
-import {ProjectFormsComponent} from './project-forms/project-forms.component'
-import {ProjectPublicationsComponent} from './project-publications/project-publications.component'
-import {ProjectUsersComponent} from './project-users/project-users.component'
-import {ProjectDashboardComponent} from './project-dashboard/project-dashboard.component'
-import {PubReposComponent} from "./pub-repos/pub-repos.component";
-import {PipesComponent} from "./pipes/pipes.component";
-import {RenderComponent} from "../../core/codebooks/render/render.component";
+import { Routes } from "@angular/router";
+import { MainComponent } from "./main.component";
+import { SettingsComponent } from "./settings/settings.component";
+import { ProfileComponent } from "./settings/profile/profile.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { CreateProjectComponent } from "./create-project/create-project.component";
+import { ProjectFormPageComponent } from "./project-form/project-form-page.component";
+import { FormBuilderComponent } from "./form-builder/form-builder.component";
+import { PubCoderComponent } from "./pub-coder/pub-coder.component";
+import { ConflictsComponent } from "./conflicts/conflicts.component";
+import { DiscussComponent } from "./discuss/discuss.component";
+import { MyTasksPageComponent } from "./my-tasks-page/my-tasks-page.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { ProjectFormsComponent } from './project-forms/project-forms.component'
+import { ProjectPublicationsComponent } from './project-publications/project-publications.component'
+import { ProjectUsersComponent } from './project-users/project-users.component'
+import { ProjectDashboardComponent } from './project-dashboard/project-dashboard.component'
+import { PubReposComponent } from "./pub-repos/pub-repos.component";
+import { RenderComponent } from "../../core/codebooks/render/render.component";
 
 export const mainRoutes: Routes = [
-    {
-      path: '',
-      component: MainComponent,
-      children: [
-        {
-          path: 'pub-coder-4',
-          loadChildren: './pub-coder-4/pub-coder-4.module#PubCoder4Module'
-        },
-        {
-          path: 'settings',
-          component: SettingsComponent,
-          children: [
-            {
-              path: 'profile',
-              component: ProfileComponent
-            },
-            {
-              path: '',
-              redirectTo: 'profile',
-              pathMatch: 'full'
-            }
-          ]
-        },
-        {
-          path: "welcome",
-          component: WelcomeComponent
-        },
-        {
-          path: 'render-codebook',
-          component: RenderComponent
-        },
-        {
-          path: 'projects',
-          component: ProjectsComponent
-        },
-        {
-          path: 'create-project',
-          component: CreateProjectComponent
-        },
-        {
-          path: "project-dashboard",
-          component: ProjectDashboardComponent
-        },
-        {
-          path: "project-users",
-          component: ProjectUsersComponent
-        },
-        {
-          path: "project-publications",
-          component: ProjectPublicationsComponent
-        },
-        {
-          path: 'project-codebooks',
-          component: ProjectFormsComponent
-        },
-        {
-          path: "projects/:pid/forms/:fid",
-          component: ProjectFormPageComponent
-        },
-        {
-          path: 'forms/:id',
-          component: FormBuilderComponent
-        },
-        {
-          path: 'pub-coder/:id',
-          component: PubCoderComponent
-        },
-        {
-          path: "conflicts/:id",
-          component: ConflictsComponent
-        },
-        {
-          path: "discuss",
-          component: DiscussComponent
-        },
-        {
-          path: "tasks",
-          component: MyTasksPageComponent
-        },
-        {
-          path: 'pub-repos',
-          component: PubReposComponent
-        },
-        {
-          path: 'pipelines',
-          component: PipesComponent
-        },
-        {
-          path: '',
-          redirectTo: 'project-dashboard',
-          pathMatch: 'full'
-        }
-      ]
-    }
-  ];
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'pub-coder-4',
+        loadChildren: './pub-coder-4/pub-coder-4.module#PubCoder4Module'
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent,
+        children: [
+          {
+            path: 'profile',
+            component: ProfileComponent
+          },
+          {
+            path: '',
+            redirectTo: 'profile',
+            pathMatch: 'full'
+          }
+        ]
+      },
+      {
+        path: "welcome",
+        component: WelcomeComponent
+      },
+      {
+        path: 'render-codebook',
+        component: RenderComponent
+      },
+      {
+        path: 'projects',
+        component: ProjectsComponent
+      },
+      {
+        path: 'create-project',
+        component: CreateProjectComponent
+      },
+      {
+        path: "project-dashboard",
+        component: ProjectDashboardComponent
+      },
+      {
+        path: "project-users",
+        component: ProjectUsersComponent
+      },
+      {
+        path: "project-publications",
+        component: ProjectPublicationsComponent
+      },
+      {
+        path: 'project-codebooks',
+        component: ProjectFormsComponent
+      },
+      {
+        path: "projects/:pid/forms/:fid",
+        component: ProjectFormPageComponent
+      },
+      {
+        path: 'forms/:id',
+        component: FormBuilderComponent
+      },
+      {
+        path: 'pub-coder/:id',
+        component: PubCoderComponent
+      },
+      {
+        path: "conflicts/:id",
+        component: ConflictsComponent
+      },
+      {
+        path: "discuss",
+        component: DiscussComponent
+      },
+      {
+        path: "tasks",
+        component: MyTasksPageComponent
+      },
+      {
+        path: 'pub-repos',
+        component: PubReposComponent
+      },
+      {
+        path: '',
+        redirectTo: 'project-dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  }
+]
